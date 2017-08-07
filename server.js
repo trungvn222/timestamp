4bcf2b514060854df20279bc16f50a13fa336ccb
trungvn222 <ngoquangtrung.it@gmail.com>
2017-08-07 21:39:03 +0700

    update first

---
+++ server.js
@@ -0,0 +1,52 @@
+// server.js
+// where your node app starts
+
+// init project
+var express = require('express');
+var app = express();
+
+// we've started you off with Express, 
+// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("/", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+app.get("/:date", function(req, res){
+  var date = new Date(req.params.date);
+  var dateF = date.toLocaleTimeString("en-us", {  
+  weekday: "short", year: "numeric", month: "long"
+  });
+  var data = {
+    "unix": date.getTime(),
+    "natural": dateF
+  };
+   res.setHeader('Content-Type', 'application/json');
+  res.send(JSON.stringify(data));
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
+
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
